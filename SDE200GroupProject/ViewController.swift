8974324fb41b8aeec06ceb8b329ba1e96463a4ec
Stephen Harr <sharr01@my.blueridgectc.org>
2024-10-24 14:24:03 -0400

    Added card functions and card assets

--- SDE200GroupProject/ViewController.swift
+++ SDE200GroupProject/ViewController.swift
@@ -13,8 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
-}
+    } // End of View Controller
+    
+    func createDeck()-> ([String], [Int]){
+            // each suit of a card
+            let suits = ["hearts", "spades", "clubs", "diamonds"]
+            // each value name of the card
+            let ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+            // Values of each card
+            let values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+            // new variable for array of cards created with loop
+            var deckOfCards = [String]()
+            var valuesArray = [Int]()
+            
+            // Loop to create the deck with concatenation of string names to match values of assets
+            for suit in suits{
+                for (index, rank) in ranks.enumerated(){
+                    deckOfCards.append("\(rank)_of_\(suit)")
+                    if rank == "ace"{
+                        valuesArray.append(1) // Create additional logic to change this value as needed.
+                    } else {
+                        valuesArray.append(values[index])
+                    }
+                }
+            }
+            return (deckOfCards, valuesArray)
+        } // End of createDeck function
+    
+    func pickRandomCard(deckOfCards: [String], valueArray: [Int]) -> (String, Int){
+        let randomIndex = Int(arc4random_uniform(UInt32(deckOfCards.count)))
+        let randomCard = deckOfCards[randomIndex]
+        let cardValue = valueArray[randomIndex]
+        
+        return (randomCard, cardValue)
+    } // End of pickRandomCard function
+    
+} // End of main class
 
